993aaec9abe3e678124a4094478b6d067ed5c3a9
kitallis <kitallis@users.noreply.github.com>
2023-02-12 18:13:39 +0000

    deploy: 84a3ba112d239275a64486e2898fbefc534b5376

--- macigè-870dece4a7f1137e.js
+++ macigè-870dece4a7f1137e.js
@@ -774,7 +774,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('macigè-2854145b569d6d1b_bg.wasm', import.meta.url);
+        input = new URL('macigè-870dece4a7f1137e_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
