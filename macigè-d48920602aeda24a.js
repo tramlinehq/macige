87943fff0beeb4dd96cf27c53289a86c7d1c9259
pratul <pratul@users.noreply.github.com>
2023-02-21 07:56:28 +0000

    deploy: 4702f06da6bf6e85e369285129ff6b17cfe8d495

--- macigè-d48920602aeda24a.js
+++ macigè-d48920602aeda24a.js
@@ -774,7 +774,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('macigè-8cbad595485ae4b_bg.wasm', import.meta.url);
+        input = new URL('macigè-d48920602aeda24a_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
